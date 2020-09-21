401d231f00baa9ab1494dc72d829209af45c32c8
Steve Goldhaber <goldy@ucar.edu>
2020-09-20 22:45:55 -0600

    New table format

--- src/hash/hist_hash_table.F90
+++ src/hash/hist_hash_table.F90
@@ -93,9 +93,7 @@ module hist_hash_table
 
    type, abstract, public :: hist_hashable_t
       ! The hashable type is a base type that contains a hash key.
-      integer, private :: hash_key_len = 0
    contains
-      procedure                                  :: key_len
       procedure(hist_hashable_get_key), deferred :: key
    end type hist_hashable_t
 
@@ -104,6 +102,17 @@ module hist_hash_table
    contains
       procedure :: key => hist_hashable_char_get_key
    end type hist_hashable_char_t
+
+   public :: new_hashable_char
+
+   type, public, extends(hist_hashable_t) :: hist_hashable_int_t
+      integer, private :: value
+   contains
+      procedure :: key => hist_hashable_int_get_key
+      procedure :: val => hist_hashable_int_get_val
+   end type hist_hashable_int_t
+
+   public :: new_hashable_int
 
    integer, parameter :: gen_hash_key_offset = 21467 ! z'000053db'
 
@@ -114,29 +123,30 @@ module hist_hash_table
    integer, parameter :: table_factor_size = 8     ! Table size / # entries
    integer, parameter :: table_overflow_factor = 4 ! # entries / Overflow size
 
-   type :: hash_overflow_entry
-      ! An entry for a hash overflow table
-      integer, pointer :: hentry(:) => NULL()
-   end type hash_overflow_entry
+   type :: table_entry_t
+      ! Any table entry contains a key and a value
+      class(hist_hashable_t), pointer             :: entry_value => NULL()
+      type(table_entry_t),    pointer             :: next => NULL()
+   contains
+      final :: finalize_table_entry
+   end type table_entry_t
 
    type, public :: hist_hash_table_t
       ! hist_hash_table_t contains all information to build and use a hash table
       ! It also keeps track of statistics such as collision frequency and size
-      integer, private              :: table_size = -1
-      integer, private              :: overflow_size = -1
-      integer, private              :: key_offset = gen_hash_key_offset
-      integer, private, allocatable :: primary_table(:)
-      integer, private              :: next_overflow_index = 1
-      type(hash_overflow_entry), private, allocatable :: overflow_table(:)
-      ! Statistics
-      integer, private :: num_keys = 0
-      integer, private :: num_key_collisions = 0
-      integer, private :: max_collision = 0
+      integer, private                          :: table_size = -1
+      integer, private                          :: overflow_size = -1
+      integer, private                          :: key_offset = gen_hash_key_offset
+      type(table_entry_t), private, allocatable :: primary_table(:)
+ ! Statistics
+      integer, private                          :: num_keys = 0
+      integer, private                          :: num_key_collisions = 0
+      integer, private                          :: max_collision = 0
    contains
       procedure :: initialize   => hash_table_initialize_table
       procedure :: key_hash     => hash_table_key_hash
       procedure :: add_hash_key => hash_table_add_hash_key
-      procedure :: table_index  => hash_table_table_index
+      procedure :: table_value  => hash_table_table_value
    end type hist_hash_table_t
 
    ! Abstract interface for key procedure of hist_hashable class
@@ -151,11 +161,13 @@ module hist_hash_table
 CONTAINS
 
    !#######################################################################
-   integer function key_len(this)
-      class(hist_hashable_t), intent(in) :: this
 
-      key_len = this%hash_key_len
-   end function key_len
+   function new_hashable_char(name_in)
+      type(hist_hashable_char_t)   :: new_hashable_char
+      character(len=*), intent(in) :: name_in
+
+      new_hashable_char%name = name_in
+   end function new_hashable_char
 
    !#######################################################################
 
@@ -169,12 +181,55 @@ CONTAINS
 
    !#######################################################################
 
-   subroutine hash_table_initialize_table(this, tbl_size, ovflw_size, key_off)
+   function new_hashable_int(value_in)
+      type(hist_hashable_int_t) :: new_hashable_int
+      integer, intent(in)       :: value_in
+
+      new_hashable_int%value = value_in
+   end function new_hashable_int
+
+   !#######################################################################
+
+   function hist_hashable_int_get_key(hashable)
+      ! Return the hashable int class key (value ==> string)
+      class(hist_hashable_int_t), intent(in) :: hashable
+      character(len=:), allocatable          :: hist_hashable_int_get_key
+
+      character(len=32) :: key_str
+
+      write(key_str, '(i0)') hashable%val()
+      hist_hashable_int_get_key = trim(key_str)
+   end function hist_hashable_int_get_key
+
+   !#######################################################################
+
+   integer function hist_hashable_int_get_val(hashable)
+      ! Return the hashable int class value
+      class(hist_hashable_int_t), intent(in) :: hashable
+
+      hist_hashable_int_get_val = hashable%value
+   end function hist_hashable_int_get_val
+
+   !#######################################################################
+
+   subroutine finalize_table_entry(te)
+      type(table_entry_t)          :: te
+
+      nullify(te%entry_value) ! We do not own the memory
+      if (associated(te%next)) then
+         deallocate(te%next) ! This should invoke finalize recursively
+         nullify(te%next)
+      end if
+
+   end subroutine finalize_table_entry
+
+   !#######################################################################
+
+   subroutine hash_table_initialize_table(this, tbl_size, key_off)
       ! Initialize this table.
       ! Dummy arguments
       class(hist_hash_table_t)      :: this
       integer,           intent(in) :: tbl_size   ! new table size
-      integer,           intent(in) :: ovflw_size ! overflow table size
       integer, optional, intent(in) :: key_off    ! key offset
       ! Local variable
       integer                       :: index
@@ -183,20 +238,8 @@ CONTAINS
       if (allocated(this%primary_table)) then
          deallocate(this%primary_table)
       end if
-      if (allocated(this%overflow_table)) then
-         do index = 1, size(this%overflow_table, 1)
-            if (associated(this%overflow_table(index)%hentry)) then
-               deallocate(this%overflow_table(index)%hentry)
-               nullify(this%overflow_table(index)%hentry)
-            end if
-         end do
-         deallocate(this%overflow_table)
-      end if
       this%table_size = tbl_size
       allocate(this%primary_table(this%table_size))
-      this%primary_table = 0
-      this%overflow_size = ovflw_size
-      allocate(this%overflow_table(this%overflow_size))
       if (present(key_off)) then
          this%key_offset = key_off
       end if
@@ -230,7 +273,7 @@ CONTAINS
 
       hash = this%key_offset
       ind_fact = 0
-      do index = 1, len(string)
+      do index = 1, len_trim(string)
          ind_fact = ind_fact + 1
          if (ind_fact > tbl_max_idx) then
             ind_fact = 1
@@ -245,120 +288,116 @@ CONTAINS
 
    !#######################################################################
 
-   integer function hash_table_table_index(this, hash_arr, key,               &
-        errmsg) result(indx)
+   function hash_table_table_value(this, key, errmsg) result(tbl_val)
       !
       !-----------------------------------------------------------------------
       !
-      ! Purpose: Return the the index of <key> in <hash_arr>
+      ! Purpose: Return the the key value of <key>
       !
-      !          If the object is not found, return -1.
-      !
-      !          <hash_arr> is an array of hashable objects
-      !          For correct usage, all of the keys in <hash_arr>
-      !          MUST have been added to <this> table.
+      !          If the object is not found, return NULL
       !
       !-----------------------------------------------------------------------
       !
       !  Arguments:
       !
-      class(hist_hash_table_t)                :: this
-      class(hist_hashable_t),     intent(in)  :: hash_arr(:)
-      character(len=*),           intent(in)  :: key
-      character(len=*), optional, intent(out) :: errmsg
+      class(hist_hash_table_t)                      :: this
+      character(len=*),                 intent(in)  :: key
+      character(len=*),       optional, intent(out) :: errmsg
+      class(hist_hashable_t), pointer               :: tbl_val
       !
       !  Local.
       !
-      integer          :: hash_key
-      integer          :: ovflw_cind ! location of number of overflow entries
-      integer          :: ind_ovflw  ! Index of overflow chain in overflow table
-      integer          :: num_ovflw  ! Number of entries on overflow chain
-      integer          :: test_ind   ! Test index for overflow entries
-      integer, pointer :: ovflw(:)   ! Overflow chain
-      character(len=*), parameter :: subname = 'HASH_TABLE_TABLE_INDEX'
+      integer                        :: hash_key
+      type(table_entry_t), pointer   :: next_ptr
+      character(len=*),    parameter :: subname = 'HASH_TABLE_TABLE_INDEX'
 
       errmsg = ''
+      nullify(tbl_val)
       hash_key = this%key_hash(key)
-      indx = this%primary_table(hash_key)
-      if (indx < 0) then
-         ovflw_cind = abs(indx)
-         ovflw => this%overflow_table(ovflw_cind)%hentry
-         if (.not. associated(ovflw)) then
-            if (present(errmsg)) then
-               write(errmsg, *) subname, ": Empty overflow entry for '",      &
-                    trim(key), "'"
-            end if
-         else
-            num_ovflw = size(ovflw, 1)
-            do ind_ovflw = 1, num_ovflw
-               test_ind = ovflw(ind_ovflw)
-               if (hash_arr(test_ind)%key() == key) then
-                  indx = test_ind
+      if (this%primary_table(hash_key)%entry_value%key() == trim(key)) then
+         tbl_val => this%primary_table(hash_key)%entry_value
+      else
+         next_ptr => this%primary_table(hash_key)%next
+         do
+            if (associated(next_ptr)) then
+               if (next_ptr%entry_value%key() == trim(key)) then
+                  tbl_val => next_ptr%entry_value
                   exit
                end if
-            end do
-         end if
+               next_ptr => next_ptr%next
+            else
+               exit
+            end if
+         end do
       end if
 
-      if ((indx <= 0) .and. present(errmsg)) then
-         if (indx == 0) then
-            write(errmsg, *) subname, ": No entry for '", trim(key), "'"
-         else if (len_trim(errmsg) == 0) then
-            write(errmsg, *) subname, ": No overflow entry for '",            &
-                 trim(key), "'"
-         end if ! No else, we have already recorded an error
-      else if ((hash_arr(test_ind)%key() /= key) .and. present(errmsg)) then
-         write(errmsg, *) subname, ": error finding field '", trim(key), "'"
+      if ((.not. associated(tbl_val)) .and. present(errmsg)) then
+         write(errmsg, *) subname, ": No entry for '", trim(key), "'"
       end if
 
-   end function hash_table_table_index
+   end function hash_table_table_value
 
    !#######################################################################
 
-   subroutine hash_table_add_hash_key(this, key, keyval)
+   subroutine hash_table_add_hash_key(this, newval, errmsg)
       !
       !-----------------------------------------------------------------------
       !
-      ! Purpose: Add <keyval> to this hash table using key, <key>
+      ! Purpose: Add <newval> to this hash table using its key
+      !          Its key must not be an empty string
+      !          It is an error to try to add a key more than once
       !
       !
       !-----------------------------------------------------------------------
 
       !  Dummy arguments:
-      class(hist_hash_table_t)                :: this
-      character(len=*),           intent(in)  :: key
-      integer,                    intent(in)  :: keyval
+      class(hist_hash_table_t)                      :: this
+      class(hist_hashable_t), pointer               :: newval
+      character(len=*),       optional, intent(out) :: errmsg
       ! Local variables
-      integer                     :: hash_ind
-      integer                     :: ovflw_ind
-      integer                     :: ovflw_len
-      integer,          pointer   :: curr_ovflw(:)
-      character(len=*), parameter :: subname = 'HASH_TABLE_ADD_HASH_KEY'
+      integer                          :: hash_ind
+      integer                          :: ovflw_len
+      character(len=:),    allocatable :: newkey
+      type(table_entry_t), pointer     :: next_ptr
+      type(table_entry_t), pointer     :: new_entry
+      character(len=*),    parameter   :: subname = 'HASH_TABLE_ADD_HASH_KEY'
 
-      hash_ind = this%key_hash(key)
-      if (this%primary_table(hash_ind) == 0) then
-         ! No entry, add the entry here
-         this%primary_table(hash_ind) = keyval
-      else if (this%primary_table(hash_ind) < 0) then
-         ! We already have a collision here, add a new one
-         ovflw_ind = abs(hash_ind)
-         curr_ovflw => this%overflow_table(ovflw_ind)%hentry
-         ovflw_len = size(curr_ovflw, 1) + 1
-         allocate(this%overflow_table(ovflw_ind)%hentry(ovflw_len))
-         this%overflow_table(ovflw_ind)%hentry(1:ovflw_len-1) = curr_ovflw(:)
-         this%overflow_table(ovflw_ind)%hentry(ovflw_len) = keyval
-         this%max_collision = MAX(this%max_collision, ovflw_len)
+      newkey = newval%key()
+      hash_ind = this%key_hash(newkey)
+      ! Check for this entry
+      if (associated(this%table_value(newkey))) then
+         if (present(errmsg)) then
+            write(errmsg, *) subname, " ERROR: key, '", newkey,               &
+                 "' already in table"
+         end if
       else
-         ! We have a new collision, create an overflow entry
-         ovflw_ind = this%next_overflow_index
-         this%next_overflow_index = this%next_overflow_index + 1
-         ovflw_len = 2
-         allocate(this%overflow_table(ovflw_ind)%hentry(ovflw_len))
-         this%overflow_table(ovflw_ind)%hentry(1) = this%primary_table(hash_ind)
-         this%overflow_table(ovflw_ind)%hentry(ovflw_len) = keyval
-         this%max_collision = MAX(this%max_collision, ovflw_len)
-         this%num_key_collisions = this%num_key_collisions + 1
-         this%primary_table(hash_ind) = -ovflw_ind
+         ASSOCIATE(tbl_entry => this%primary_table(hash_ind))
+            if (associated(tbl_entry%entry_value)) then
+               ! We have a collision, make a new entry
+               allocate(new_entry)
+               new_entry%entry_value => newval
+               ! Now, find a spot
+               if (associated(tbl_entry%next)) then
+                  ovflw_len = 1
+                  next_ptr => tbl_entry%next
+                  do
+                     if (associated(next_ptr%next)) then
+                        ovflw_len = ovflw_len + 1
+                        next_ptr => next_ptr%next
+                     else
+                        exit
+                     end if
+                  end do
+                  ovflw_len = ovflw_len + 1
+                  next_ptr%next => new_entry
+               else
+                  this%num_key_collisions = this%num_key_collisions + 1
+                  tbl_entry%next => new_entry
+                  ovflw_len = 1
+               end if
+               this%max_collision = MAX(this%max_collision, ovflw_len)
+            end if
+         END ASSOCIATE
       end if
       this%num_keys = this%num_keys + 1
 
--- test/test_hash.F90
+++ test/test_hash.F90
@@ -1,10 +1,45 @@
-program test_hash
+module test_hash_utils
    use hist_hash_table, only: hist_hashable_t, hist_hashable_char_t
-   use hist_hash_table, only: hist_hash_table_t
+   use hist_hash_table, only: new_hashable_char
 
+   implicit none
+   private
+
+   public :: new_hash_char_array
+
+CONTAINS
+
+   subroutine new_hash_char_array(keys, hash_arr)
+      character(len=*),                        intent(in)  :: keys(:)
+      type(hist_hashable_char_t), allocatable, intent(out) :: hash_arr(:)
+
+      integer :: index
+
+      allocate(hash_arr(size(keys, 1)))
+      do index = 1, size(hash_arr, 1)
+         hash_arr(index) = new_hashable_char(keys(index))
+      end do
+   end subroutine new_hash_char_array
+
+end module test_hash_utils
+
+program test_hash
+   use hist_hash_table, only: hist_hashable_char_t, hist_hash_table_t
+   use test_hash_utils, only: new_hash_char_array
+
+   character(len=10) :: hash_names(4) = (/ 'foo       ', 'bar       ',        &
+        'foobar    ', 'big daddy ' /)
    type(hist_hashable_char_t), allocatable :: hash_chars(:)
    type(hist_hash_table_t)                 :: hash_table
+   integer                                 :: index
+   character(len=1024)                     :: errors
+   character(len=128)                      :: errmsg
 
-   call hash_table%initialize(10, 5)
+   errors = ''
+   call new_hash_char_array(hash_names, hash_chars)
+   call hash_table%initialize(10)
+   do index = 1, size(hash_chars, 1)
+      call hash_table%add_hash_key(hash_chars(index))
+   end do
 
 end program test_hash
